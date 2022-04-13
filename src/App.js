be107874307f463ff5175b6a838be77d677451a8
camkaley <camkaley@gmail.com>
2022-04-13 11:46:31 +1000

    added base files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Login from "./views/login"
+
+function App() {
+  return (
+    <div className="App">
+      <Login>
+        
+      </Login>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/login/index.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import "./index.css";
+
+function Login(props) {
+  const [authed, setAuthed] = useState(false);
+
+  useEffect(() => {}, []);
+
+  return (
+    <div className="container">
+        <div className="loginText">
+            test
+        </div>
+    </div>
+  );
+}
+export default Login;
